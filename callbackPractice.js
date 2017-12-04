98fe97d5be6c69836807872e8458d92616e7b544
Gabriella <gaba@Gabriellas-MBP.attlocal.net>
2017-12-03 19:57:42 -0600

    need help with the last 3 problems

--- callbackPractice.js
+++ callbackPractice.js
@@ -17,8 +17,10 @@ and WHAT YOU SHOULD WRITE is the sayHi function that makes the code above work:
 
 
 // 1. Write a function called first that takes in two parameters, an array and a callback function, then invokes the callback function, passing in the first element in the array as it's argument.  (see the sayHi function on line 12 for reference)
-
-  // Code Here 
+ 
+var first = function(arr, cb) {
+  cb(arr[0]);
+}
 
   
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -32,7 +34,9 @@ first(names, function(firstName){
 
 // 2. Write a function called last that takes in an array and a callback function, then invokes the callback, passing in the last element in the array as the argument.
 
-  //Code Here
+  var last = function(arr, cb){
+    cb(arr.slice(-1)[0]);
+  }
 
 
 last(names, function(lastName){
@@ -44,7 +48,9 @@ last(names, function(lastName){
 
 // 3. Write a function called multiply that takes in three parameters: two numbers and a callback function.  Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 
-  //Code Here
+  var multiply = function (num1, num2, cb) {
+    cb(num1 * num2);
+  }
 
 
 multiply(4, 3, function(answer){
@@ -57,7 +63,10 @@ multiply(4, 3, function(answer){
 // If it does, invoke the callback with true as the argument. 
 // If the name does not exist, invoke the callback with false as the argument.
 
-  //Code Here 
+  var contains = function (arr, named, cb) {
+    cb(true);
+      cb(false);
+  }
 
 
 
@@ -75,8 +84,9 @@ contains(names, 'Colt', function(result){
 // 5. Write a function called uniq that takes in an array and a callback function.
 // Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
 
-  //Code Here
-
+ 
+  
+  
 
 
 uniq(names, function(uniqArr){
@@ -86,8 +96,7 @@ uniq(names, function(uniqArr){
 
 // 6. Write a function called each that takes in an array of names and a callback function. For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 
-    //Code Here 
-
+ 
 
 
 each(names, function(item, indice){
